874967ce5c01d46b5593f725b23f5b991e1565d8
fluk-code <ferreirafelipericardo@gmail.com>
2022-07-09 12:32:02 -0300

    feat: Create category repository

---
+++ src/category/domain/repositories/category.repository.ts
@@ -0,0 +1,31 @@
+import { 
+  ISearchableRepository, 
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult
+} from '@seedwork/domain/repository/repository-contracts';
+import { Category } from '../entities/category';
+
+namespace CategoryRepository {
+  export type Filter = string;
+
+  export class SearchParams 
+    extends DefaultSearchParams<
+      Filter
+    > {}
+
+  export class SearchResult 
+    extends DefaultSearchResult<
+      Category, 
+      Filter
+    > {}
+
+  export interface Repository 
+    extends ISearchableRepository<
+      Category,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {} 
+}
+
+export default CategoryRepository;

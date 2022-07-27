154ae5a78b7a4bcd978aa7bf5e85e9b3db6a068c
fluk-code <ferreirafelipericardo@gmail.com>
2022-07-26 23:02:29 -0300

    chore: configure typescript and test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,25 @@
 
 export default {
   clearMocks: true,
+  coverageDirectory: "../__coverage",
+  // covarageReporters: [
+  //   "json",
+  //   "html",
+  // ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    },
+    // "./src/category/domain": {
+    //   statements: 10,
+    //   branches: 10,
+    //   functions: 10,
+    //   lines: 10
+    // }
+  },
   coverageProvider: "v8",
   rootDir: "src",
   testRegex: ".*\\..*spec\\.ts$",

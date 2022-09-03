9b02fb290b254204eec29a349a0dd7d877f01fec
fluk-code <ferreirafelipericardo@gmail.com>
2022-09-03 10:04:30 -0300

    test: Teste método create do controller de categoria

--- src/nestjs/src/categories/categories.controller.spec.ts
+++ src/nestjs/src/categories/categories.controller.spec.ts
@@ -1,20 +1,42 @@
+import { CreateCategoryUseCase } from '@fc/Core_AdmCatalogoVideo/category/application';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
-describe('CategoriesController', () => {
+const CategoriesControllerName = CategoriesController.name;
+
+describe(`${CategoriesControllerName} Unit Tests`, () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CategoriesController],
-      providers: [CategoriesService],
-    }).compile();
-
-    controller = module.get<CategoriesController>(CategoriesController);
+    controller = new CategoriesController();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should create a category', () => {
+    const expectedOutput: CreateCategoryUseCase.Output = {
+      id: 'some-valid-uuid',
+      name: 'some name',
+      description: 'some description',
+      isActive: true,
+      createdAt: new Date(),
+    };
+
+    const mockCreateUseCase = {
+      execute: jest.fn().mockReturnValue(expectedOutput),
+    };
+
+    const input: CreateCategoryDto = {
+      name: 'some name',
+      description: 'some description',
+      isActive: true,
+    };
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-expect-error
+    controller['createUseCase'] = mockCreateUseCase;
+    const cateoryOutput = controller.create(input);
+
+    expect(mockCreateUseCase.execute).toBeCalledTimes(1);
+    expect(mockCreateUseCase.execute).toBeCalledWith(input);
+    expect(cateoryOutput).toStrictEqual(expectedOutput);
   });
 });

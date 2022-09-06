434b0143000e0c3acbf8442e6658145a7db05eb4
fluk-code <ferreirafelipericardo@gmail.com>
2022-09-05 22:23:58 -0300

    test: garantir mapeamento das propriedade do categoryModel

--- src/@core/src/category/infra/db/sequelize/category.model.spec.ts
+++ src/@core/src/category/infra/db/sequelize/category.model.spec.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize-typescript';
+import { DataType, Sequelize } from 'sequelize-typescript';
 import { CategoryModel } from './category.model';
 
 const categoryModelName = CategoryModel.name;
@@ -29,6 +29,52 @@ describe(`${categoryModelName} Unit Test`, () => {
   afterAll(async () => {
     await sequelize.close();
   })
+
+  it('should mapping properties with correct values', () => {
+    const attributes = Object.keys(CategoryModel.getAttributes());
+    expect(attributes).toStrictEqual(['id', 'name', 'description', 'isActive', 'createdAt'])
+    
+    const attributesMap = CategoryModel.getAttributes();
+    
+    const idAttribute = attributesMap.id;
+    expect(idAttribute).toMatchObject({
+      field: 'id',
+      fieldName: 'id',
+      primaryKey: true,
+      type: DataType.UUID()
+    });
+
+    const nameAttribute = attributesMap.name;
+    expect(nameAttribute).toMatchObject({
+      field: 'name',
+      fieldName: 'name',
+      allowNull: false,
+      type: DataType.STRING(255)
+    });
+
+    const descriptionAttribute = attributesMap.description;
+    expect(descriptionAttribute).toMatchObject({
+      field: 'description',
+      fieldName: 'description',
+      type: DataType.TEXT()
+    });
+
+    const isActiveAttribute = attributesMap.isActive;
+    expect(isActiveAttribute).toMatchObject({
+      field: 'isActive',
+      fieldName: 'isActive',
+      allowNull: false,
+      type: DataType.BOOLEAN()
+    });
+
+    const createdAtAttribute = attributesMap.createdAt;
+    expect(createdAtAttribute).toMatchObject({
+      field: 'createdAt',
+      fieldName: 'createdAt',
+      allowNull: false,
+      type: DataType.DATE()
+    });
+  });
 
   it('should create model with correct values', async () => {
     const input = {

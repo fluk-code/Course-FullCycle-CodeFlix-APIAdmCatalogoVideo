a9bd93e385d2a293a536243ba625707436dcab8d
fluk-code <ferreirafelipericardo@gmail.com>
2023-01-09 13:47:43 -0300

    refactor: change test inside tests folder

--- src/@core/src/category/infra/db/sequelize/__tests__/category-model.int.spec.ts
+++ src/@core/src/category/infra/db/sequelize/__tests__/category-model.int.spec.ts
@@ -1,5 +1,5 @@
 import { DataType, Sequelize } from 'sequelize-typescript';
-import { CategoryModel } from './category-model';
+import { CategoryModel } from '../category-model';
 
 describe('Category Model Unit Test', () => {
   let sequelize: Sequelize;
